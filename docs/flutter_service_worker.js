460960e5990a57e9402d4b936635401278383a12
unknown <SistemasDaniel@DanielSistemas.LOAGROS.COM>
2025-10-23 14:17:15 -0400

    Deploy web build to docs

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -41,7 +41,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a547f354cd75ab06278744cdf3393336
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d38285f1d714c91b339c79fcb557a36b",
+"flutter_bootstrap.js": "9ab3e2a5d6f6755b0389671cd2b51ade",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
